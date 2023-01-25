a1f29581eeb935d50352a2978f54dbb5ed2c8e46
alperkaan01 <88709260+alperkaan01@users.noreply.github.com>
2023-01-25 13:24:01 +0300

    Finalize addding coming soon to pages except homepage

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Container, InnerStyleContainer, PortfolioMainImage } from "../Home/HomepageElements";
+
+import portfolioImage from "../../img/portfolio.png";
+import { ComingSoon } from "../Projects/ProjectsElements";
 
 const About = () => {
   return (
-    <h1>Coming Soon</h1>
+    <Container>
+        <InnerStyleContainer>
+        <PortfolioMainImage
+            src={portfolioImage}
+            alt="portfolio Image"
+        ></PortfolioMainImage>
+        </InnerStyleContainer>
+        <ComingSoon>Coming Soon</ComingSoon>
+    </Container>
   )
 }
 
---
+++ src/Pages/Projects/ProjectsElements.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const ComingSoon = styled.h1`
+    position: fixed;
+    top: 37%;
+    left: 20%;
+    font-family: cursive;
+    font-size: 3.5rem;
+`
